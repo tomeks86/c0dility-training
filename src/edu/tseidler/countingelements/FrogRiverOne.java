c9b32fed454dc136cfc74f32b08b387fde862de5
Tomasz S <tomasz.z.seidler@gmail.com>
2018-03-13 13:47:35 +0100

    FogRiverOne with bit flags

--- src/edu/tseidler/countingelements/FrogRiverOne.java
+++ src/edu/tseidler/countingelements/FrogRiverOne.java
@@ -1,16 +1,17 @@
 package edu.tseidler.countingelements;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 public class FrogRiverOne {
     public int solution(int X, int[] A) {
-        Set<Integer> nums = new TreeSet<>();
+        int count = X;
+        boolean[] flags = new boolean[X + 1];
         int i = 0;
-        while (nums.size() != X && i < A.length) {
-            nums.add(A[i]);
+        while (count > 0 && i < A.length) {
+            if (!flags[A[i]]) {
+                flags[A[i]] = true;
+                count--;
+            }
             i++;
         }
-        return nums.size() == X ? i-1 : -1;
+        return count > 0 ? -1 : i - 1;
     }
 }
